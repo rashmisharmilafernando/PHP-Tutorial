c6f55f95656d5766d5825ba065933224364b8144
Rashmi Sharmila Fernando <108237108+rashmisharmilafernando@users.noreply.github.com>
2024-11-27 10:29:43 +0530

    Variables

---
+++ veriable.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?php
+        $name = "John";
+        $age = 35;
+
+        echo "There once was a man named $name <br>";
+        echo "He was $age years old<br>";
+        echo "He really liked the name $name<br>";
+        echo "but didn't like being $age <br>";
+        echo "but didn't like being $age <br>";
+    ?>
+
+
+</body>
+
+</html>
